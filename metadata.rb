07e2a22f2041369e7648df69ee1bb356f56c8a0b
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 14:29:45 +0200

    Added dependencies on libkrb5support, libkeyutils.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libkeyutils", ">= 0.0.1"
+depends          "libkrb5support", ">= 0.0.1"
